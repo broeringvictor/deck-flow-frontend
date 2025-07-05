916f26cff640b5787fcb20d592f110038047b5b1
Broering <victor@victorbroering.adv.br>
2025-07-05 15:43:28 -0300

    criação do menu

--- deck-flow/src/app/components/menu/menu.component.ts
+++ deck-flow/src/app/components/menu/menu.component.ts
@@ -2,14 +2,12 @@ import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
-
-// Imports dos Módulos do Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { NgIf } from '@angular/common'; // Importe o NgIf se for usar *ngIf
+
 
 @Component({
   selector: 'app-menu',
